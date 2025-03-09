8b1ab8e173a1853b11fbe9f43008c7df43f325bd
sanam1999 <shresthasanam288@gmail.com>
2025-03-09 08:12:47 +0530

    dfsaf

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    Secret: "b836e93a0624e3215e6e637ff73cde72d5dc81507db515d027f94a617210", // Your secret key
+    Key: "67c94cf6a07f2ab570e42189",  // Your API key
+    folder: "dfsafsf",
+  };
+const  params = {
+    allowedFormats: ["png", "jpg", "jpeg"], 
+}
+module.exports = {config,params};
+
+// npm install axios form-data multer 
+
+// or 
+
+// breve install axios form-data multer
